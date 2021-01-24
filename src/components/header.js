4d340df361f724b50d50dd2970dc89e4fa0f3775
Dan Perlman <daniel@insituware.com>
2021-01-24 11:03:26 -0500

    update

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,37 @@
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { TabNavigation, Tab } from 'evergreen-ui'
+import { globalHistory as history } from '@reach/router'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+/**
+ * COLOR PALETTE
+ * DEE5E5
+ * 9DC5BB
+ * 17B890
+ * 5E807F
+ * 082D0F
+ * @param {} param0 
+ */
+const Header = () => {
+  console.log(history.location.pathname);
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `#17B890`,
+        marginBottom: `1rem`,
+        padding: '10px'
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+      <TabNavigation>
+        {[{loc: '/', display: 'Home'}, {loc: '/page-2', display: 'App'}, {loc:'/404', display: 'Donate'}].map(({loc, display}, index) => (
+          <Tab key={loc} is="a" href={loc} id={loc} isSelected={history.location.pathname === loc}>
+            {display}
+          </Tab>
+        ))}
+      </TabNavigation>
+    </header>
+  )
 }
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,16 @@
 import React from "react"
 import { navigate } from "gatsby"
-import { Button } from "evergreen-ui"
+import { Button, Heading, Paragraph } from "evergreen-ui"
 import Layout from "src/components/layout"
-import Image from "src/components/image"
 import SEO from "src/components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi world</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Button onClick={() => navigate('/page-2/')}>Next</Button>
+    <Heading size={900}>Financial Freedom</Heading>
+    <Paragraph>Discover your best time to buy a house, retire, etc.</Paragraph>
+
+    <Button marginTop={12} onClick={() => navigate('/page-2/')}>Begin</Button>
   </Layout>
 )
 
